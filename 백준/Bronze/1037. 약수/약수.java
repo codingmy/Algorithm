8b022b0df70332aa0803d0584f0037cd865a46b0
min0 <97686638+codingmy@users.noreply.github.com>
2024-10-19 19:14:56 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,33 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        if (n == 1) {
+            int out = Integer.parseInt(st.nextToken());
+            System.out.println(out * out);
+        } else {
+            int a = 0;
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                a = Integer.parseInt(st.nextToken());
+                if (i == 0) {
+                    min = a;
+                    max = a;
+                } else if (min > a) {
+                    min = a;
+                } else if (max < a) {
+                    max = a;
+                }
+
+            }
+            System.out.println(min * max);
+        }
+    }
+}
